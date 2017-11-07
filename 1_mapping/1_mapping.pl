62cd2b08899d213e1fabb94fdecc797d1aff4f05
sfverbru <steven_verbruggen@ugent.be>
2017-11-07 12:19:39 +0100

    Bugfix

--- 1_mapping/1_mapping.pl
+++ 1_mapping/1_mapping.pl
@@ -388,7 +388,7 @@ $run_name = $run_name."_".$mapper."_".$unique."_".$ensemblversion;
 
 #Conversion for species terminology
 my $spec = (uc($species) eq "MOUSE") ? "Mus_musculus" : (uc($species) eq "RAT") ? "Rattus_norvegicus" : (uc($species) eq "SL1344") ? "SL1344" : (uc($species) eq "HUMAN") ? "Homo_sapiens" : (uc($species) eq "ARABIDOPSIS") ? "Arabidopsis_thaliana" : (uc($species) eq "FRUITFLY") ? "Drosophila_melanogaster" : (uc($species) eq "YEAST") ? "Saccharomyces_cerevisiae" : (uc($species) eq "ZEBRAFISH") ? "Danio_rerio" : "";
-my $spec_short = (uc($species) eq "MOUSE") ? "mmu" : (uc("RAT")) ? "rnor" : (uc($species) eq "SL1344") ? "sl1344" : (uc($species) eq "HUMAN") ? "hsa" : (uc($species) eq "ARABIDOPSIS") ? "ath" : (uc($species) eq "FRUITFLY") ? "dme" : (uc($species) eq "YEAST") ? "sce" : (uc($species) eq "ZEBRAFISH") ? "dre" : "";
+my $spec_short = (uc($species) eq "MOUSE") ? "mmu" : (uc($species) eq "RAT") ? "rnor" : (uc($species) eq "SL1344") ? "sl1344" : (uc($species) eq "HUMAN") ? "hsa" : (uc($species) eq "ARABIDOPSIS") ? "ath" : (uc($species) eq "FRUITFLY") ? "dme" : (uc($species) eq "YEAST") ? "sce" : (uc($species) eq "ZEBRAFISH") ? "dre" : "";
 #Old mouse assembly = NCBIM37, new one is GRCm38. Old human assembly = GRCh37, the new one is GRCh38
 my $assembly = (uc($species) eq "MOUSE" && $ensemblversion >= 70 ) ? "GRCm38"
 : (uc($species) eq "MOUSE" && $ensemblversion < 70 ) ? "NCBIM37"
