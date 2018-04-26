516b702a7834d61f90b2d5c42665789a5c191b27
sfverbru <steven_verbruggen@ugent.be>
2018-04-26 17:00:10 +0200

    sra parallel downloader: first release

---
+++ Information/download_sra_parallel.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+## This script uses parallel-fastq-dump (conda install parallel-fastq-dump) to download for multiple SRR entries at once
+## nohup ./download_sra_parallel.sh -f 3034567 -l 3034572 > nohup.txt &
+
+#Read input parameters
+FIRST=
+LAST=
+while [ "$1" != "" ]; do
+    case $1 in
+        -f )        shift
+                    FIRST=$1
+                    ;;
+        -l )        shift
+                    LAST=$1
+                    ;;
+    esac
+    shift
+done
+
+echo
+echo
+echo "PARALLEL FASTQ DUMP"
+echo
+echo "Getting fastq files for SRR$FIRST up to SRR$LAST"
+echo
+
+mkdir tmp
+for ((i=$FIRST; i<=$LAST; i++))
+do
+	echo "Downloading SRR$i"
+	parallel-fastq-dump -s SRR$i -t 20 -O . --tmpdir tmp
+done
+rm -rf tmp
