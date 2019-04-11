e6b0760b0284352141daaf3403392955dbd35409
sfverbru <Steven.Verbruggen@UGent.be>
2019-04-11 12:33:48 +0200

    STAR indexation for Salmonella: bugfix

--- 1_mapping/mapping.pl
+++ 1_mapping/mapping.pl
@@ -1214,7 +1214,7 @@ sub check_STAR_index {
     my $starIndexDirComplete = $ref_loc.$starIndexDir;
     my $genomeSAindexNbases;
 
-	if ($species eq 'MYC_ABS_ATCC_19977'){
+	if ($species eq 'MYC_ABS_ATCC_19977' || $species eq 'SL1344'){
 		$genomeSAindexNbases = 4;
 	}
 	else {
